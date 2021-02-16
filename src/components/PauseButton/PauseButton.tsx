f390080b77d6e407d06fb9b0fc672ef3c772105c
Mikołaj Kujawa <mkkujawix@gmail.com>
2021-02-16 19:43:04 +0100

    PAS-4 Cleanup

--- src/components/PauseButton/PauseButton.tsx
+++ src/components/PauseButton/PauseButton.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import { View} from 'react-native'
+import React from 'react'
+import { View } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { ViewType } from '../../types/styles'
 
 import styles from './PauseButton.scss'
 
-const PauseButton = () => {
-    return ( 
-        <View style={styles.box as ViewType} >            
-            <Icon name="pause" type='antdesign' color='#fff' size={100}/>            
-        </View>
-    );
-}
- 
-export default PauseButton;
+const PauseButton = () => (
+		<View style={styles.box as ViewType}>
+			<Icon name='pause' type='antdesign' color='#fff' size={100} />
+		</View>
+	)
+
+export default PauseButton
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { TextType, ViewType } from '../../types/styles'
 import { Icon } from 'react-native-elements'
+import { TextType, ViewType } from '../../types/styles'
 
 import styles from './Home.scss'
 
-import ProgressBar from '../../components/ProgressBar/ProgressBar';
-import WavyHeader from '../../components/WavyHeader/WavyHeader';
-import PauseButton from '../../components/PauseButton/PauseButton';
+import ProgressBar from '../../components/ProgressBar/ProgressBar'
+import WavyHeader from '../../components/WavyHeader/WavyHeader'
+import PauseButton from '../../components/PauseButton/PauseButton'
 
 const Home = () => (
 	// const { useSubscribe } = useSettingsContext()
@@ -15,19 +15,19 @@ const Home = () => (
 	// const translations = useSubscribe((s) => s.translations)
 
 	<View style={styles.container as ViewType}>
-			<WavyHeader/>
-			<View style={styles.pauseButton as ViewType}>
-				<PauseButton/>
-			</View>
+		<WavyHeader />
+		<View style={styles.pauseButton as ViewType}>
+			<PauseButton />
+		</View>
 
-			<View style={styles.progressBar as ViewType}>
-				<ProgressBar maxValue={1000} currentValue={300} barColor='#F2B077'/>
-				<View style={styles.bottomInfo as ViewType}>
-					<Icon name='account' type='material-community' color='#fff' size={50} />
-					<Text style={styles.levelText as TextType}>Poziom 3</Text>
-					<Icon name='cog-outline' type='material-community' color='#fff' size={50}/>
-				</View>
-			</View>		
+		<View style={styles.progressBar as ViewType}>
+			<ProgressBar maxValue={1000} currentValue={300} barColor='#F2B077' />
+			<View style={styles.bottomInfo as ViewType}>
+				<Icon name='account' type='material-community' color='#fff' size={50} />
+				<Text style={styles.levelText as TextType}>Poziom 3</Text>
+				<Icon name='cog-outline' type='material-community' color='#fff' size={50} />
+			</View>
+		</View>
 	</View>
 )
 
