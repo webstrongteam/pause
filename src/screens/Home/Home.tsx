88d65e618e815b4bdb6f9a471c805155609d20ac
Mikołaj Kujawa <mkkujawix@gmail.com>
2021-02-19 20:09:16 +0100

    PAS-4_Create_Home Repair WavyHeader Centering PauseButton Bug fix

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { TextType, ViewType } from '../../types/styles'
 import {NavigationScreenType} from '../../types/navigation'
 
 import styles from './Home.scss'
 
-import ProgressBar from '../../components/ProgressBar/ProgressBar'
 import WavyHeader from '../../components/WavyHeader/WavyHeader'
 import PauseButton from '../../components/PauseButton/PauseButton'
+import Footer from '../../components/Footer/Footer';
 
 type Props={
 	navigation:NavigationScreenType
@@ -17,19 +17,18 @@ type Props={
 const Home = ({navigation}:Props) => (
 
 	<View style={styles.container as ViewType}>
-		<WavyHeader />
-		<View style={styles.pauseButton as ViewType}>
-			<PauseButton />
-		</View>
-
-		<View style={styles.progressBar as ViewType}>
-			<ProgressBar maxValue={1000} currentValue={300} barColor='#F2B077' />
-			<View style={styles.bottomInfo as ViewType}>
+		<View style={{position:"absolute", top:0, width:"100%"}}><WavyHeader variant="centered"/></View>
+			<View style={{height:"100%"}}>
+				<TouchableOpacity style={styles.pauseButton as ViewType}>
+					<PauseButton />
+				</TouchableOpacity>
+			</View>
+		
+			<Footer currentValue={300} maxValue={1000} barColor="#F2B077">
 				<Icon name='account' onPress={()=>navigation.navigate("Profile")} type='material-community' color='#fff' size={50} />
 				<Text style={styles.levelText as TextType}>Poziom 3</Text>
 				<Icon name='cog-outline' onPress={()=>navigation.navigate("Settings")} type='material-community' color='#fff' size={50} />
-			</View>
-		</View>
+			</Footer>				
 	</View>
 )
 
