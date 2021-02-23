2c8c51683de523c519067f5a9df77c3874f4290a
Mikołaj Kujawa <mkkujawix@gmail.com>
2021-02-23 21:49:33 +0100

    PAS-5 Add Icons

--- src/screens/PauseScreen/PauseScreen.tsx
+++ src/screens/PauseScreen/PauseScreen.tsx
@@ -1,38 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { TextType, ViewType } from '../../types/styles'
+import { NavigationScreenType } from '../../types/navigation'
 
 import WavyHeader from '../../components/WavyHeader/WavyHeader';
 import CloseIcon from '../../components/UI/CloseIcon/CloseIcon';
 
 import styles from './PauseScreen.scss';
+
+type Props = {
+	navigation: NavigationScreenType
+}
  
-const PauseScreen = () => {
+const PauseScreen = ({ navigation }: Props) => {
     return ( 
         <View style={styles.container as ViewType}>
-            <WavyHeader/>
-            <Text>
-                <Text>ćwiczenie</Text>
-                <Text>Wall Sit</Text>
-            </Text>
-            <Text>
-                <Text>
-                    <Text>Czas trwania</Text>
-                    <Text>30s x4</Text>
-                </Text>
-                <Text>
-                    <Text>Muzyka</Text>
-                    <Text>Sea</Text>
-                </Text>                             
-            </Text>
-            <View style={styles.bottomIcons as ViewType}>
-                <View style={styles.randomIcon as ViewType}>
-                    <Icon name='random' type='FontAwsome' color='#fff' size={50} />
+            <View style={{position:"absolute", top:-40, width:"100%"}}>
+                <WavyHeader variant="centered">
+                    <CloseIcon onPress={() => navigation.goBack()}/>          
+                    <View style={styles.exerciseTitle as ViewType}>
+                        <Text style={{ fontSize:15, textTransform:'uppercase', color:'#1a6a73' }}>ćwiczenie</Text>
+                        <Text style={{ fontSize:40, textTransform:'uppercase', textDecorationLine:'underline', color:'#1a6a73' }}>Wall Sit</Text>
+                    </View>
+                </WavyHeader>
+            </View>
+            
+
+            <View style={{ position:'absolute', top:'50%', width:'100%' }}>
+
+                <View style={styles.exerciseInfo as ViewType}>
+                    <Text style={styles.firstInfo as TextType}>Czas trwania</Text>
+                    <Text style={[styles.firstInfo, styles.secondInfo] as TextType}>30s x4</Text>
                 </View>
-                <View style={styles.playIcon as ViewType}>
-                    <Icon name='play' type='Feather' color='#fff' size={50} />
-                </View>                             
+
+                <View style={styles.exerciseInfo as ViewType}>
+                    <Text style={styles.firstInfo as TextType}>Muzyka</Text>
+                    <Text style={[styles.firstInfo, styles.secondInfo] as TextType}>Sea</Text>
+                </View> 
+
+            </View>
+
+            <View style={styles.bottomIcons as ViewType}>
+
+                <View style={styles.Icon as ViewType}>
+                    <TouchableOpacity>
+                        <Icon name='random' type='font-awesome' color='#fff' size={50} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.Icon as ViewType}>
+                    <TouchableOpacity>
+                        <Icon name='play' type='feather' color='#fff' size={50} />
+                    </TouchableOpacity>
+                </View>
+
             </View>
         </View>
     );
